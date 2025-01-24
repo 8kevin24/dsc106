e09c003e8e74ea8d7e4d1d72deef7bf44d5cdab7
8kevin24 <lab820028@gmail.com>
2025-01-24 12:48:21 -0800

    'lmao'

--- global.js
+++ global.js
@@ -3,3 +3,35 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let pages = [
+    { url: '', title: 'Kevin Zhu: Personal Site and Portfolio' },
+    { url: 'projects/', title: 'Kevin\'s Portfolio' },
+    {url: 'contact/', title: 'Kevin\'s contact'},
+    {url: 'resume/', title: 'Kevin\'s Resume'}
+  ];
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  for (let p of pages) {
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    let url = p.url;
+    let title = p.title;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+      }
+      
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    if (a.host !== location.host) {
+        a.target="_blank";
+    }
+      
+      
+  }
+  
+  
